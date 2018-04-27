ac14d5f334ecd08c457c73b90b0f81b0b16a5ee1
Ed <3winner3@gmail.com>
2018-04-27 16:54:04 +0300

    first commit

---
+++ src/isomorph.service.ts
@@ -0,0 +1,107 @@
+export class IsomorphService {
+	isDefined(value) {
+		return typeof value !== 'undefined';
+	}
+
+	isObject(value) {
+		const type = typeof value;
+
+		return value != null && (type === 'object' || type === 'function');
+	}
+
+	loop(num, func) {
+		for (let i = 0; i < num; i++) {
+			func(i);
+		}
+	}
+
+	loopReverse(num, func) {
+		for (let i = num; i > 0; i--) {
+			func(i);
+		}
+	}
+
+	delFromList(list, elem) {
+		if (Array.isArray(list)) {
+			this.delFromArr(list, elem);
+		} else {
+			for (let key in list) {
+				let objElem = list[key];
+
+				if (objElem === elem) {
+					delete list[key];
+					break;
+				}
+			}
+		}
+	}
+
+	inList(list, val) {
+		let result;
+
+		if (Array.isArray(list)) {
+			result = (list.indexOf(val) > -1) ? true : false;
+		} else {
+			for (let key in list) {
+				if (list[key] === val) {
+					result = true;
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	delFromArr(arr: any[], elem) {
+		const index = arr.indexOf(elem);
+
+		if (index > -1) {
+			arr.splice(index, 1);
+		}
+	}
+
+	getFirstProp(list) {
+		return list[this.getFirstKey(list)];
+	}
+
+	getFirstKey(list) {
+		return Array.isArray(list) ? 0 : Object.keys(list)[0];
+	}
+
+	getKeyByValue(obj, value) {
+		for (let key in obj) {
+			if (obj[key] === value) {
+				return key;
+			}
+		}
+	};
+
+	isEven(num) {
+		return num % 2 === 0;
+	}
+
+	makeUnixTimeStamp(date) {
+		return Math.round(+date / 1000);
+	}
+
+	getMonthName(date) {
+		return date.toLocaleString('en-us', {month: 'long'});
+	}
+
+	capitalize(str) {
+		return str.charAt(0).toUpperCase() + str.slice(1);
+	}
+
+	clearList(list) {
+		if (!list) { return; }
+
+		if (Array.isArray(list)) {
+			list.length = 0;
+		} else {
+			for (let key in list) {
+				delete list[key];
+			}
+		}
+	}
+}
